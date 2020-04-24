cc0567873e993ba6a6610e214ab88783bc94f5c2
anoopjprakash <anoop.jprakash@gmail.com>
2020-04-23 20:42:19 -0400

    Initial Commit

---
+++ src/test/java/com/learning/app/SpringReactApp/SpringReactAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.app.SpringReactApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

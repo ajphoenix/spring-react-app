502c3e4087247547f4063105663aabf48a409cf1
anoopjprakash <anoop.jprakash@gmail.com>
2020-04-27 19:18:52 -0400

    Added docker build and docker compose files

--- src/test/java/com/learning/app/SpringReactApp/SpringReactAppApplicationTests.java
+++ src/test/java/com/learning/app/SpringReactApp/SpringReactAppApplicationTests.java
@@ -1,13 +1,9 @@
 package com.learning.app.SpringReactApp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringReactAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
